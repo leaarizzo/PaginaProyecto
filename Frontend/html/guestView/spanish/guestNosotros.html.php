f6f577f270121c4c04da0975a2493c20b83f9fcb
Daertle <nachologoedu@gmail.com>
2024-10-07 09:37:51 -0300

    nosotros

--- Frontend/html/guestView/spanish/guestNosotros.html.php
+++ Frontend/html/guestView/spanish/guestNosotros.html.php
@@ -1,19 +1,17 @@
-<?php include '../../../../BackEnd/Gestion de Usuarios/verificarpermisos4.php'; ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Luxury Driving</title>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
+<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Luxury Driving</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="Frontend/css/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
   <link rel="stylesheet" href="../../../css/style.css" />
 </head>
 
@@ -21,8 +19,8 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-black fixed-top">
     <div class="container">
       <a href="../../../../index.html.php">
-        <img src="../../../img/logo.png" alt="Logo" width="200" height="67" > 
-    </a>
+        <img src="../../../img/logo.png" alt="Logo" width="200" height="67">
+      </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar5"
         aria-controls="navbar5" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -43,7 +41,7 @@
             <a href="guestContacto.html.php" class="nav-link">Contacto</a>
           </li>
           <li class="nav-item">
-            <a href="guestChoferes.html.php" class="nav-link">Choferes</a> 
+            <a href="guestChoferes.html.php" class="nav-link">Choferes</a>
           </li>
           <li class="nav-item">
             <a href="guestTests.html.php" class="nav-link">Tests</a>
@@ -51,7 +49,7 @@
           <li class="nav-item">
             <a href="guestRequisitos.html.php" class="nav-link">Requisitos</a>
           </li>
-          
+
           <li class="nav-item dropdown">
             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-globe"></i></a>
             <ul class="dropdown-menu">
@@ -74,28 +72,55 @@
   </nav>
 
 
-  <div>
-    <h1 class="hola">NOSOTROS</h1>
+
+
+  <h1 class="titulo"> Academia de Choferes Luxurydriving</h1>
+
+  <div class="contentNos">
+    <div>
+        <h2 class="Subtitulo">Visión</h2>
+        <p class="info">Ser reconocidos como la empresa líder en clases de conducción en Uruguay, distinguiéndonos por nuestro compromiso con la excelencia, la innovación y la adaptación a las necesidades de nuestros alumnos. Aspiramos a ser un referente nacional en la enseñanza de conducción, estableciendo los más altos estándares en la formación de conductores y contribuyendo al desarrollo de una sociedad más segura en las vías.</p>
+        
+        <h2 class="Subtitulo">Propósito</h2>
+        <p class="info">Nuestro propósito es empoderar a los uruguayos para que alcancen su máximo potencial como conductores, brindándoles las herramientas y conocimientos necesarios para dominar el arte de la conducción. Nos enfocamos en cultivar su confianza, habilidades técnicas y comprensión de las normas de tránsito, al mismo tiempo que fomentamos un profundo respeto por la seguridad vial y la responsabilidad al volante. Nuestro objetivo es formar conductores competentes y comprometidos con la prevención de accidentes, promoviendo así un entorno más seguro para todos los usuarios de las vías en Uruguay.</p>
+        
+        <h2 class="Subtitulo">Misión</h2>
+        <p class="info">Nuestra misión es brindar una formación integral y de alta calidad en conducción a todos los uruguayos, fomentando una cultura de seguridad vial y respeto por las normas de tránsito. Nos esforzamos por formar conductores responsables, hábiles y seguros, capaces de enfrentar cualquier situación en las carreteras con confianza y profesionalismo.</p>
+    
+      </div>
+
+    <img src="../../../img/nosotros.png" class="imgNos">
   </div>
 
+  
+  <footer class="site-footer">
+    <div class="container">
+      <div class="footer-content">
+        <ul class="ul1">
+          <li class="li1"><a class="a1" href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
+          <li class="li1"><a class="a1" href="#"><i class="fab fa-facebook-f" aria-hidden="true"></i></a></li>
+          <li class="li1"><a class="a1" href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a></li>
+          <li class="li1"><a class="a1" href="#"><i class="fab fa-linkedin-in" aria-hidden="true"></i></a></li>
+          <li class="li1"><a class="a1" href="#"><i class="fab fa-youtube" aria-hidden="true"></i></a></li>
+        </ul>
+        
+        <div class="footer-abajo">
+          <a href="#" class="footer-link">Servicios</a>
+          <a href="guestLandingSpanish.html" class="footer-logo">
+            <img src="../../../img/logo.png" alt="Logo" width="200" height="67">
+          </a>
+          <a href="#" class="footer-link">Contactanos</a>
+        </div>
+      </div>
+    </div>
+  </footer>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-crossorigin="anonymous"></script>
-<script src="../../../js/script.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+    crossorigin="anonymous"></script>
+  <script src="../../../js/script.js"></script>
 </body>
 
 </html>
